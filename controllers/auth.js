bbc3333b6d7a077595a1c19e280397c23792a78b
MateusSNeubarth <mateusneubarth@hotmail.com>
2023-11-17 17:17:52 -0300

    added isAdmin on auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,14 +30,18 @@ export const login = async (req, res, next) => {
         if(!isPasswordCorrect) 
             return next(createError(400, "Wrong password or username"));
 
-        const token = jwt.sign({ id:user._id, isAdmin:user.isAdmin }, process.env.JWT);
+        const token = jwt.sign(
+            { id:user._id, isAdmin:user.isAdmin }, 
+            process.env.JWT
+        );
 
         const { password, isAdmin, ...otherDetails } = user._doc;
-        res.cookie("access_token", token, {
-            httpOnly: true,
-        })
-        .status(200)
-        .json({ ...otherDetails });
+        res.
+            cookie("access_token", token, {
+                httpOnly: true,
+            })
+            .status(200)
+            .json({ details: { ...otherDetails }, isAdmin });
     } catch (err) {
         next(err);
     }
