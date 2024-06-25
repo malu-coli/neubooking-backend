a14d79dc6129781caaecea6689229ccd93af041e
Mateus S. Neubarth <mateusneubarth@hotmail.com>
2024-06-25 01:06:20 -0300

    fixed ATC_002 test case

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use("/api/rooms", roomsRoute);
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "Something went wrong";
-  return res.status(500).json({
+  return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
     message: errorMessage,
